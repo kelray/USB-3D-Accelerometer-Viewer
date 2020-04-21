71c814192d9d04ab918ab2bfa33ae2294eff1c57
El-Rayes <kelray@users.noreply.github.com>
2020-04-21 13:08:06 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@
  *
  * The views and conclusions contained in the software and documentation are
  * those of the authors and should not be interpreted as representing official
- * policies, either expressed or implied, of Hong Xu.
+ * policies, either expressed or implied, of the author.
  *
  *
  *
